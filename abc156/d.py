dd2e74b5ebf2e3bc65893fd6e1b7d33c8a7b488e
Kamiya <kamiya826@gmail.com>
2020-10-15 16:52:56 +0900

    Commit

--- abc156/d.py
+++ abc156/d.py
@@ -15,18 +15,11 @@ n, a, b = map(int, input().split())
 
 mod = 10 ** 9 + 7
 
-two = [1] * (10 ** 5 + 1)
-for i in range(1, 10 ** 5 + 1):
-    two[i] = two[i - 1] * 2 % mod
-if n > 10 ** 5:
-    two = two[10 ** 5] * two[n % (10 ** 5)] % mod
-else:
-    two = two[n]
+two = pow(2, n, mod)
 
 comb = 1
 for i in range(a):
-    comb = comb * (n - i)
-    comb = comb
+    comb = comb * (n - i) % mod
 comb_a = comb
 for i in range(a, b):
     comb = comb * (n - i) % mod
@@ -39,5 +32,7 @@ for i in range(a, b):
     t = t * (i + 1) % mod
 B = t
 
+comb_a = comb_a * pow(A, mod - 2, mod) % mod
+comb_b = comb_b * pow(B, mod - 2, mod) % mod
 
 print((two - comb_a - comb_b - 1) % mod)
--- abc156/e.py
+++ abc156/e.py
@@ -3,10 +3,25 @@ import sys
 
 # input here
 _INPUT = """\
+15 6
 
 """
 sys.stdin = io.StringIO(_INPUT)
 
 
 
+n, k = map(int, input().split())
 
+mod = 10 ** 9 + 7
+
+if k >= n - 1:
+    bunsi = 1
+    for i in range(n - 1):
+        bunsi = bunsi * (2 * n - 1 - i) % mod
+    bunbo = 1
+    for i in range(n - 1):
+        bunbo = bunbo * (i + 1) % mod
+    comb = bunsi * pow(bunbo, mod - 2, mod) % mod
+    print(comb)
+else:
+    
