edcfed47de8d98c2d18bfae7c94603dec1268d55
n-nooobu <kamiya826@gmail.com>
2022-01-26 23:01:02 +0900

    commit

--- abc235/c.py
+++ abc235/c.py
@@ -3,10 +3,35 @@ import sys
 
 # input here
 _INPUT = """\
+6 8
+1 1 2 3 1 2
+1 1
+1 2
+1 3
+1 4
+2 1
+2 2
+2 3
+4 1
 
 """
 sys.stdin = io.StringIO(_INPUT)
 
 
 
+from collections import defaultdict
 
+N, Q = map(int, input().split())
+a = list(map(int, input().split()))
+
+dic = defaultdict(list)
+for i in range(N):
+    dic[a[i]].append(i + 1)
+
+for q in range(Q):
+    x, k = map(int, input().split())
+
+    if len(dic[x]) >= k:
+        print(dic[x][k - 1])
+    else:
+        print(-1)
--- abc235/d.py
+++ abc235/d.py
@@ -9,4 +9,18 @@ sys.stdin = io.StringIO(_INPUT)
 
 
 
+a, N = map(int, input().split())
 
+dp = [[0] * 10 ** 6 for _ in range(20)]
+dp[0][1] = 1
+
+for i in range(19):
+    for j in range(10 ** 6):
+        if dp[i][j]:
+            if dp[i][j] * a < 10 ** 6:
+                dp[i + 1][dp[i][j] * a] = 1
+            dp[i + 1][int(str(j)[-1] + str(j)[:-1])] = 1
+
+for j in range(20):
+    if dp[N][j]:
+        print()
